84af5e5b28b47732414036fbf8b92700c04eff3b
matveib <one_two_2021@mail.ru>
2020-04-14 22:24:55 +0300

    lab 4

--- index.php
+++ index.php
@@ -9,8 +9,22 @@ require_once("matvei/Linear.php");
 require_once("matvei/Square.php");
 require_once("matvei/Log.php");
 
-$solver = new matvei\Square();
-$roots = $solver->solve(1, 2, -3);
-matvei\Log::log("roots: " . implode(" ", $roots));
+$co_arr = [];
+foreach(["a", "b", "c"] as $co) {
+	echo "Enter ".$co.": ";
+	$line = stream_get_line(STDIN, 1024, PHP_EOL);
+	$co_arr[$co] = $line === "" ? 0 : $line;
+}
+$a = $co_arr["a"];
+$b = $co_arr["b"];
+$c = $co_arr["c"];
+try {
+  $solver = new matvei\Square();
+	matvei\Log::log("Equation: $a*x^2 + $b*x + $c = 0");
+	matvei\Log::log("roots: " . implode(" ", $solver->solve($a, $b, $c)));
+}catch(matvei\matveiException $ex) {
+	matvei\Log::log($ex->getMessage());
+}
+
 matvei\Log::write();
 ?>
